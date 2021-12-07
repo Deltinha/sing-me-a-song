0785f1e0c074d880f535931dc221860c586b0892
Bruno <mguimaraesbruno@gmail.com>
2021-12-07 14:39:43 -0300

    Code Review: Brunão

--- dump.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "recommendations" (
-	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"youtubeLink" varchar(255) NOT NULL,
-	"score" integer NOT NULL,
-	CONSTRAINT "recommendations_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
--- src/middlewares/errorHandler.js
+++
@@ -1,4 +0,0 @@
-export default async function errorHandler(err, req, res, next) {
-  console.error(err);
-  return res.sendStatus(500);
-}
